8e1e7849a9b98cf56bca95547a716d4a4932b88b
aeon33system <aeon33system@gmail.com>
2019-05-13 13:40:09 +0800

    update script

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -2,8 +2,14 @@
 
 $(aws ecr get-login --no-include-email --registry-ids 204065533127 --region ap-northeast-1)
 
-docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/chatbot_db:latest
-docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/chatbot_flask:latest
-docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/chatbot_serveo:latest
-
+if [ `docker images | wc -l` > 1 ]
+then
+	docker image rm -f 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/chatbot_db:latest
+	docker image rm -f 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/chatbot_flask:latest
+	docker image rm -f 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/chatbot_serveo:latest
+else
+	docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/chatbot_db:latest
+	docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/chatbot_flask:latest
+	docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/chatbot_serveo:latest
+fi
 exit 0
