626e8ddc7f0af2ce5e316ec2d5ce9c913277f20e
aeon33system <aeon33system@gmail.com>
2019-05-13 14:19:57 +0800

    update dockercompose ln

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+#sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 cd /home/ec2-user/deploy/code/
 aws s3 cp s3://dc101-project02/secretFile.txt .
