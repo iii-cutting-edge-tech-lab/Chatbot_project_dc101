ec590abc9463c91f892afe4d0eccc43c206b9dff
aeon33system <aeon33system@gmail.com>
2019-05-13 17:04:22 +0800

    update awslog

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-#sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo yum update -y
+sudo yum install -y awslogs
+sudo sed -i 's/us-east-1/ap-northeast-1/g' /etc/awslogs/awscli.conf
+sudo sed -i 's/\/var\/log\/messages/user.log/g' /etc/awslogs/awslogs.conf
+sudo sed -i 's/file = user.log/file = \/home\/ec2-user\/deploy\/code\/logs\/*.log/g' /etc/awslogs/awslogs.conf
+sudo sed -i 's/log_group_name = user.log/log_group_name = \/project2_user-dialog/g' /etc/awslogs/awslogs.conf
+sudo service awslogs restart
+
 
 cd /home/ec2-user/deploy/code/
 aws s3 cp s3://dc101-project02/secretFile.txt .
